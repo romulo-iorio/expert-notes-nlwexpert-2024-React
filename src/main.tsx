0b2781b15a84c292180b1f1ad97e79e5e024b302
Rômulo Iorio <romuloiorio@hotmail.com>
2024-02-13 19:39:02 -0300

    setup: removing unused files

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import React from "react";
 import { App } from "./app";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
